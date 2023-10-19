de1d5110534e3580dbc0edab86a5f78b6e30adee
박승우 <qpfgud430@gmail.com>
2023-10-19 20:59:11 +0900

    feat: footer 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,32 @@
+import Image from "next/image";
+import Profile from "../../public/Profile.webp";
 export default function Home() {
   return (
     <>
-      <div className="mx-auto max-w-xl py-8">
-        <h1 className="mb-8 text-left text-2xl font-black">안녕하세요.</h1>
-        <p className="mb-4 text-left font-black">
-          프론트엔드 개발자 박승우입니다.
-        </p>
+      <div className="flex mx-auto max-w-xl py-8">
+        <Image
+          src={Profile}
+          quality={100}
+          className="h-40 w-40 rounded-full object-cover"
+          alt="Profile"
+        />
+        <div className="mx-auto max-w-xl py-8">
+          <h1 className="mb-8 text-left text-2xl font-black">안녕하세요.</h1>
+          <p className="mb-4 text-left font-black">
+            프론트엔드 개발자 박승우입니다.
+          </p>
+        </div>
       </div>
-      <div>
+      <div className="py-8">
         <p className="mb-4">
           저는 FrontEnd를 깊이 있게 다져가기 위해 양질의 글을 쓰고자
           노력하고있습니다.
         </p>
         <p className="mb-4">
           좋은 글, 다른사람에게 도움이 되고 영감을 주는 개발 글을 작성하여
+          다양한 지식을 공유하는 것을
         </p>
-        <p className="mb-4">다양한 지식을 공유하는 것이 목표입니다.</p>
+        <p className="mb-4">목표하고 있습니다.</p>
       </div>
     </>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { AiFillGithub, AiOutlineMail } from "react-icons/ai";
 
 function Footer() {
   return (
-    <div className="flex items-center justify-center w-full h-20 ">
-      © {new Date().getFullYear()}
-      &nbsp;parkseungwoo all rights reserved.
+    <div className="flex flex-col items-center justify-center w-full h-20 ">
+      <div className="flex gap-5 mb-2">
+        <div className="cursor-pointer">
+          <a href="https://github.com/parkingbox" target="blank">
+            <AiFillGithub size={25} />
+          </a>
+        </div>
+        <div
+          className="cursor-pointer"
+          onClick={() => window.open("mailto:qpfgud430@gmail.com")}
+        >
+          <AiOutlineMail size={25} />
+        </div>
+      </div>
+      <div>
+        © {new Date().getFullYear()}
+        &nbsp;parkseungwoo all rights reserved.
+      </div>
     </div>
   );
 }
