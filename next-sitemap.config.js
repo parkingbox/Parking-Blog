05e31c751f993a59da7c99b2c79b75dc131d77bb
박승우 <qpfgud430@gmail.com>
2023-11-02 19:08:58 +0900

    feat: sitemap 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://bepyan.github.io",
+  siteUrl: "https://parking-blog-three.vercel.app/",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
