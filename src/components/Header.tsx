1249cf7bae432bad6aec075f357ef33e517a433a
박승우 <qpfgud430@gmail.com>
2024-01-29 21:00:59 +0900

    scrollIndicate 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import DarkModeBtn from "./DarkModeBtn";
+import ScrollIndicate from "./ScrollIndicate";
 
 function Header() {
   return (
     <nav className="mt-10 h-14 w-full">
+      {/* 디자인 변경 후  progresbar  */}
+      {/* <ScrollIndicate /> */}
       <div className="mx-auto flex h-full w-full max-w-3xl items-center justify-between px-4 py-4">
         <h1 className="text-3xl font-bold">
           <Link href="/">ParkingBox</Link>
