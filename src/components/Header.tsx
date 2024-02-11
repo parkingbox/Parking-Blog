be4f5378fbbd5f97cc4787cf16b90c3f09fb44a0
박승우 <qpfgud430@gmail.com>
2024-02-11 22:25:14 +0900

    style: header border 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ import ScrollIndicate from "./ScrollIndicate";
 
 function Header() {
   return (
-    <nav className="fixed top-0 w-full dark:bg-neutral-900 bg-[#F7ECDC]">
+    <nav className="fixed top-0 w-full border-b dark:border-b-black border-b-gray-200 ">
       <ScrollIndicate />
-      <div className="mx-auto my-3 flex h-full w-full max-w-3xl items-center justify-between px-4 ">
+      <div className="backdrop-blur-sm mx-auto  py-3 flex h-full w-full max-w-3xl items-center justify-between px-4 ">
         <h1 className="text-3xl font-bold">
           <Link href="/">ParkingBox</Link>
         </h1>
