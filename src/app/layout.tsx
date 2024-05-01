38601f124392155b307655706783fa4c01124fb7
Seungwoo Park <101634719+parkingbox@users.noreply.github.com>
2024-05-01 16:07:39 +0900

    [style] 배경 color 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="dark:bg-neutral-800 bg-neutral-100">
+      <body className="dark:bg-custom-black bg-neutral-100">
         <Providers>
           <main className="h-full mt-28 mx-auto w-full max-w-3xl px-4">
             <Header />
